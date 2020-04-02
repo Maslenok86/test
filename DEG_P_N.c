ba438ee1942c6f0faf4bd28e00ce0b0d1499ee66
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-03 00:56:29 +0300

    Add modile P-5 (#1)
    it's not final version

---
+++ DEG_P_N.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+int* DEG_P_N(int n,int *C)
+{
+    int *res;
+    res=(int*)malloc(2*sizeof(int));
+    res[0]=(C+(n-1)*2+0);
+    res[1]=(C+(n-1)*2+1);
+    return *res;
+}
+int main(void)
+{
+    int*C;
+    int *res;
+    int m,i;
+    m=4;
+    C=(int*)malloc(m*2*sizeof(int));
+    res=(int*)malloc(2*sizeof(int));
+    for(i=0;i<m;i++)
+    {
+        printf("Enter %d number\n",i+1);
+        scanf("%d",(C+i*2+0));
+        scanf("%d",(C+i*2+1));
+    }//----------------------------------------------
+    res=DEG_P_N(m,C);
+    printf("%d\n%d",*(res+0),*(res+1));
+    free(C);
+    free(res);
+}
